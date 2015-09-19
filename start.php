37a65a06df8b2f63b10892f8430a348112e17888
iionly <iionly@gmx.de>
2015-09-19 19:09:36 +0200

    Updated to version 1.9.1; changes see CHANGES.txt

--- start.php
+++ start.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * No Logging for Elgg 1.9
+ * No Logging for Elgg 1.9 - 1.12 and Elgg 2.X
  *
  * This plugin disables the Elgg system log
  *
@@ -16,7 +16,7 @@ elgg_register_event_handler('init','system','no_logging_init');
 
 function no_logging_init(){
 
-    // disable the system log
-    elgg_unregister_event_handler('log', 'systemlog', 'system_log_default_logger');
-    elgg_unregister_event_handler('all', 'all', 'system_log_listener');
+	// disable the system log
+	elgg_unregister_event_handler('log', 'systemlog', 'system_log_default_logger');
+	elgg_unregister_event_handler('all', 'all', 'system_log_listener');
 }
